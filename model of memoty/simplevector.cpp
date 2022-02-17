ea39141e1719244e670fd27f64fe093cb1bfa323
VasiliyDenisov <mr.v.m.denisov@mail.ru>
2022-02-17 16:27:48 +0300

    Add const methoods

--- model of memoty/simplevector.cpp
+++ model of memoty/simplevector.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 template<typename T>
 class SimpleVector {
@@ -9,22 +10,27 @@ public:
         end_ = data + size;
     }
 
-    ~SimpleVector(){
-        delete[] data;
-    }
+    ~SimpleVector(){ delete[] data; }
 
     T& operator[](size_t index){
         return *(data + index);
     }
     
     T* begin(){ return data; }
-
     T* end(){ return end_; }
+
+    const T* begin() const { return data; }
+    const T* end() const { return end_; }
 
 private:
     T* data;
     T* end_;
 };
+
+template<typename T>
+void Print(const SimpleVector<T>& vec) {
+    for (auto val : vec) { std:: cout << val << ' '; }
+}
 
 int main(){
     SimpleVector<int> sv(6);
